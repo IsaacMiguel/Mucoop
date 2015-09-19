5d4fe89fda35aa0a4da6b0d6670e432acaa3f2a6
Isaac Miguel <isaacmiguel@gmail.com>
2015-09-19 20:20:40 -0300

    Update config.php

--- htdocs/application/config/config.php
+++ htdocs/application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = 'ritmoysustancia';
+$config['encryption_key'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -361,4 +361,4 @@ $config['proxy_ips'] = '';
 /* End of file config.php */
 /* Location: ./application/config/config.php */
 
-define('SITE_URL','http://mucoop.com.ar/');
+define('');
